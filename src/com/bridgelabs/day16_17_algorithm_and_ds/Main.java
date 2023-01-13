dd70a43176fd39d8dad5317a277a37a951e77431
Sourav2707 <smartsourav33@gmail.com>
2023-01-13 12:49:03 +0530

    UC1 permutations of a String using iterative method and Recursion method

--- src/com/bridgelabs/day16_17_algorithm_and_ds/Main.java
+++ src/com/bridgelabs/day16_17_algorithm_and_ds/Main.java
@@ -1,7 +1,15 @@
 package com.bridgelabs.day16_17_algorithm_and_ds;
-
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 public class Main {
+
     public static void main(String[] args) {
         System.out.println("Welcome to day 16 & 17 algorithm and data structure program");
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.next();
+        Permutation.findPermutations(str);
+        Permutation.recursivePermutation("", str);
+        Permutation.checkEqual();
     }
 }
---
+++ src/com/bridgelabs/day16_17_algorithm_and_ds/Permutation.java
@@ -0,0 +1,54 @@
+package com.bridgelabs.day16_17_algorithm_and_ds;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Permutation {
+    static List<String> partial1 = new ArrayList<>();
+    public static void findPermutations(String str)
+    {
+        if (str == null || str.length() == 0) {
+            return;
+        }
+        // initialize the list with the first character of the string
+        partial1.add(String.valueOf(str.charAt(0))); //s
+        for (int i = 1; i < str.length(); i++)
+        {
+            // consider previously constructed partial permutation one by one, iterating backwards
+            for (int j = partial1.size() - 1; j >= 0 ; j--)
+            {
+                // remove current partial permutation from the ArrayList
+                String s = partial1.remove(j); //empty
+                // Insert the next char of the specified string at all, possible positions of current partial permutation.
+                for (int k = 0; k <= s.length(); k++)
+                {
+                    partial1.add(s.substring(0, k) + str.charAt(i) + s.substring(k)); //empty+o+s//s+o+empty
+                }
+            }
+        }
+    }
+    static ArrayList<String> partial2 = new ArrayList<>();
+    public static void recursivePermutation(String fix, String str) {
+        int length = str.length();//6//5
+        if (length == 1)
+        {
+            partial2.add(fix + str); //"soura"+"v"
+        }
+        else
+        {
+            for (int i = 0; i < length; i++)
+            {
+                char[] mychar = str.toCharArray();//'s''o''u''r''a''v'//'o''u''r''a''v'//'u''r''a''v'
+                char c = mychar[i];//'s'//'o'//'u'//'r'
+                String st = str.substring(0, i) + str.substring(i + 1);//empty+"ourav"//empty+"urav"//empty+"rav"
+                recursivePermutation(fix + c, st);//"s", "ourav"//"so", "urav"//"sou", "rav"//"sour", "av"//"soura", "v"
+            }
+        }
+    }
+    public static void checkEqual() {
+        for(String value : partial2) {
+            System.out.println(value+" found in both iterative and recursive method "+partial1.contains(value));
+        }
+    }
+}
