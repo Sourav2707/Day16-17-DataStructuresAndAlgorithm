c3d41fe09a4642d0ad085cbe4f0d0f574c125dee
Sourav2707 <smartsourav33@gmail.com>
2023-01-19 19:57:20 +0530

    UC4 checking anagram for two strings

---
+++ src/com/bridgelabs/day16_17_Anagram/Anagram.java
@@ -0,0 +1,25 @@
+package com.bridgelabs.day16_17_Anagram;
+import java.util.Arrays;
+import java.util.Scanner;
+public class Anagram {
+    public void checkAnagram(String word1, String word2) {
+        char[] char1 = word1.toCharArray();
+        char[] char2 = word2.toCharArray();
+        Arrays.sort(char1);
+        Arrays.sort(char2);
+        if(char1.length == char2.length && Arrays.equals(char1, char2))
+            System.out.println(word1+" and "+word2+" are anagrams");
+        else
+            System.out.println(word1+" and "+word2+" are not anagrams");
+    }
+    public static void main(String[] args) {
+        System.out.println("Enter two anagram words to check");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter word 1");
+        String word1 = scanner.next();
+        System.out.println("Enter word 2");
+        String word2 = scanner.next();
+        Anagram anagram = new Anagram();
+        anagram.checkAnagram(word1, word2);
+    }
+}
