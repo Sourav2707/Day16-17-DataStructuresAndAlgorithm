3da77108bd159e74ecf1293a086a479a75ef1848
Sourav2707 <smartsourav33@gmail.com>
2023-01-19 20:39:25 +0530

    UC5 finding the prime numbers within the range of 1000

---
+++ src/com/bridgelabs/day16_17_PrimeNumber/Prime.java
@@ -0,0 +1,25 @@
+package com.bridgelabs.day16_17_PrimeNumber;
+
+public class Prime {
+    boolean match;
+    public void isPrime(int n) {
+        for(int i = 2; i < n; i++) { //skipped 1, 2 beacuse they are not prime
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    match = false;
+                    break;
+                }
+                else
+                    match = true;
+            }
+            if(match == true) {
+                System.out.print(i + " ");
+            }
+        }
+        }
+    public static void main(String[] args) {
+        System.out.println("The below are the list of prime numbers within the range of 1000");
+        Prime prime = new Prime();
+        prime.isPrime(1000);
+    }
+}
